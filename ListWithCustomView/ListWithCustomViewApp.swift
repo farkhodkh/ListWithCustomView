7b0b8e9ae559ec786bce06bfca9c4eefe8f11d08
farkhod <986532qw@gmail.com>
2021-10-18 21:37:33 +0300

    Initial Commit

---
+++ ListWithCustomView/ListWithCustomViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListWithCustomViewApp.swift
+//  ListWithCustomView
+//
+//  Created by Farkhod on 18.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct ListWithCustomViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
